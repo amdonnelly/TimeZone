b3eacb64507a5d3308d5cbcd32464240ad6f2616
Alan Donnelly <alan.donnelly@biteda.com>
2017-12-05 13:56:58 +0000

    starting Parser tests

---
+++ Timezone/Timezone.Core.Tests/ParserTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Timezone.Core.Tests
+{
+	[TestClass]
+	public class ParserTests
+	{
+		[TestMethod]
+		public void DisplayTime_ValidParameters()
+		{
+			Parser timeZoneParser = new Parser();
+			string expectedOutput = "The time in the UK is 9:30 and the time in Amsterdam is 10:30";
+
+
+
+			string output = timeZoneParser.DisplayTime("09:30", "Amsterdam");
+
+			Assert.AreEqual(expectedOutput, output);
+		}
+	}
+}
