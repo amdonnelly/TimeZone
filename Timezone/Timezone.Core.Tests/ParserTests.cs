cffd0865ddc856f31b40bfc13180f2cd795f888d
Alan Donnelly <alan.donnelly@biteda.com>
2017-12-05 14:05:36 +0000

    Adding parser tests for invalid arguments

--- Timezone/Timezone.Core.Tests/ParserTests.cs
+++ Timezone/Timezone.Core.Tests/ParserTests.cs
@@ -13,8 +13,35 @@ namespace Timezone.Core.Tests
 			string expectedOutput = "The time in the UK is 9:30 and the time in Amsterdam is 10:30";
 
 
-
 			string output = timeZoneParser.DisplayTime("09:30", "Amsterdam");
+
+
+			Assert.AreEqual(expectedOutput, output);
+		}
+
+		[TestMethod]
+		public void DisplayTime_InvalidTime()
+		{
+			Parser timeZoneParser = new Parser();
+			string expectedOutput = null;
+
+
+			string output = timeZoneParser.DisplayTime("xx:30", "Amsterdam");
+
+
+			Assert.AreEqual(expectedOutput, output);
+		}
+
+		[TestMethod]
+		public void DisplayTime_InvalidLocation()
+		{
+			Parser timeZoneParser = new Parser();
+			string expectedOutput = null;
+
+
+
+			string output = timeZoneParser.DisplayTime("09:30", "Glasgow");
+
 
 			Assert.AreEqual(expectedOutput, output);
 		}
