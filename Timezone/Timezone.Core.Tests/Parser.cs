f0b18b2cff46449cf7e8c5fd9de3b7f43c562971
Alan Donnelly <alan.donnelly@biteda.com>
2017-12-05 13:43:36 +0000

    Adding unit test project

---
+++ Timezone/Timezone.Core.Tests/Parser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Timezone.Core.Tests
+{
+	[TestClass]
+	public class Parser
+	{
+		[TestMethod]
+		public void DisplayTime_ValidParameters()
+		{
+		}
+	}
+}
